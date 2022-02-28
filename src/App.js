6d2f2a8f81c66c9a5a9ec3310f5714071e2bcc64
Shreyas-13-ops <73640884+Shreyas-13-ops@users.noreply.github.com>
2022-03-01 00:54:00 +0530

    Add static & main component files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
+import { useState } from "react";
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false);
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctors Appointment",
+      day: "5th Feb at 2:30pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Online Test",
+      day: "7th Feb at 5:30pm",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Shopping Mall",
+      day: "9th Mar at 1:30pm",
+      reminder: false,
+    },
+  ]);
+
+  //Add task
+  const addTask = (task) => {
+    let id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
+
+  //Delete task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  //Toggle reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header
+        onAddToggle={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No tasks assigned"
+      )}
     </div>
   );
 }
+
+// Points to remember:
+// 1. 'States' get passed down & 'Actions' gets passed up.
 
 export default App;
